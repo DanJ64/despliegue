83ca3c55b243cb5a02b349cc0d1fd626505e177b
Dan64 <futurenoird@gmail.com>
2019-02-08 03:22:39 +0100

    Corregida errata en el texto

--- includes/content/about.php
+++ includes/content/about.php
@@ -1,5 +1,5 @@
 <div class="container">
 <div class="jumbotron">
     <h1 class="display-4">Sobre este sitio</h1>
-    <p class="lead">Esto es un ejercicio de despliegue para la asignatura de despliegue aplicaciones web</p>
+    <p class="lead">Esto es un ejercicio para la asignatura de despliegue aplicaciones web</p>
 </div>
