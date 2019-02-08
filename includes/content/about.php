3446afda76f52b1a58d666581db093ffd21376ca
Dan64 <futurenoird@gmail.com>
2019-02-08 01:47:37 +0100

    Primer commit

---
+++ includes/content/about.php
@@ -0,0 +1,5 @@
+<div class="container">
+<div class="jumbotron">
+    <h1 class="display-4">Sobre este sitio</h1>
+    <p class="lead">Esto es un ejercicio de despliegue para la asignatura de despliegue aplicaciones web</p>
+</div>
